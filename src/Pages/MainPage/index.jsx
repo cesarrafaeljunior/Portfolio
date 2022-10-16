3e69bef79903816dd14a2ce6e38998c3e8d95b1c
cesarrafaeljunior <cesarrdev@gmail.com>
2022-10-16 16:00:08 -0300

    feat: carousel logic implement

--- src/Pages/MainPage/index.jsx
+++ src/Pages/MainPage/index.jsx
@@ -4,19 +4,29 @@ import { BsYoutube } from "react-icons/bs";
 import { GrFormPrevious } from "react-icons/gr";
 import { GrFormNext } from "react-icons/gr";
 
-import CrDev from "../../assets/img/crdev.jpg";
-import ReactLogo from "../../assets/img/LogoReact.png";
-import HtmltLogo from "../../assets/img/LogoHtml.png";
-import CssLogo from "../../assets/img/LogoCss.png";
 import { MainPageContainer } from "./style";
+import { techs } from "../../services/techs";
+import { useRef } from "react";
 
 export const MainPage = () => {
+  const carousel = useRef(null);
+
+  const handleLeftClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft -= carousel.current.offsetWidth;
+  };
+
+  const handleRightClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft += carousel.current.offsetWidth;
+  };
+
   return (
     <MainPageContainer>
       <section className="Section__Header">
         <div className="Box__PersonalImage">
           <figure>
-            <img src={CrDev} alt="César Rafael Photo" />
+            <img src="img/crdev.jpg" alt="César Rafael Photo" />
           </figure>
         </div>
         <div className="Box__Social__Networking">
@@ -24,6 +34,7 @@ export const MainPage = () => {
             <a href="https://github.com/cesarrafaeljunior" target="-blank">
               <BsGithub className="GitHub SocialIcon" />
             </a>
+            <p>Github</p>
           </figure>
           <figure>
             <a
@@ -32,31 +43,42 @@ export const MainPage = () => {
             >
               <BsLinkedin className="Linkedin SocialIcon" />
             </a>
+            <p>Linkedin</p>
           </figure>
           <figure>
             <a href="https://www.youtube.com/" target="-blank">
               <BsYoutube className="Youtube SocialIcon" />
             </a>
+            <p>Youtube</p>
           </figure>
         </div>
       </section>
-      <p>
-        Meu nome é césar, tenho 22 anos e sou estudante de desenvolvimento web
-        fullStack
-      </p>
+      <section className="Section__Main">
+        <p className="Biography">
+          Meu nome é césar, tenho 22 anos e sou estudante de desenvolvimento web
+          fullStack
+        </p>
+        <h2>Tecnologias</h2>
+      </section>
       <section className="carousel">
-        <ul>
-          <li>
-            <button>
-              <GrFormPrevious className="btnCarousel" />
-            </button>
-            <img src={ReactLogo} alt=" React Logo" />
-
-            <button>
-              <GrFormNext className="btnCarousel" />
-            </button>
-          </li>
+        <ul ref={carousel}>
+          {techs.map((tech) => {
+            return (
+              <li key={tech.name}>
+                <div className="Tech__Info">
+                  <img src={tech.img} alt=" React Logo" />
+                  <p className="Name__Tech">{tech.name}</p>
+                </div>
+              </li>
+            );
+          })}
         </ul>
+        <button type="button" className="btnPrevious" onClick={handleLeftClick}>
+          <GrFormPrevious className="btnCarousel" />
+        </button>
+        <button type="button" className="btnNext" onClick={handleRightClick}>
+          <GrFormNext className="btnCarousel" />
+        </button>
       </section>
     </MainPageContainer>
   );
